bfb1076d67799f1a67f75d24a0907f5ffa31c988
fingers0fury <catflights@gmail.com>
2018-01-10 16:09:56 -0700

    Added code to access the my_grades section of blackboard. Currently figuring out how to properly convert the url to use in beautifulsoup

--- scrape.py
+++ scrape.py
@@ -2,6 +2,15 @@ import selenium
 
 from selenium import webdriver
 
+import time
+
+import bs4
+
+from bs4 import BeautifulSoup
+
+import requests
+
+
 address = "https://blackboard.boisestate.edu/"
 
 username = ""
@@ -13,17 +22,33 @@ driver = webdriver.Chrome()
 url = driver.get(address)
 
 username_field = driver.find_element_by_id("user_id")
-
 username_field.clear()
-
 username_field.send_keys(username)
 
 password_field = driver.find_element_by_id("password")
-
 password_field.clear()
-
 password_field.send_keys(password)
 
 login_button = driver.find_element_by_id("entry-login")
-
 login_button.click()
+
+time.sleep(1.0)
+
+databases = driver.find_element_by_link_text("Sp18 - CS 410/510 - Databases")
+databases.click()
+my_grades = driver.find_element_by_link_text("My Grades")
+my_grades.click()
+
+time.sleep(1.0)
+
+r = requests.get("http://" + driver.current_url)
+
+data = r.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+grades = soup.find("div", {"id", "grades_wrapper"})
+
+print(grades)
+
+driver.close()
